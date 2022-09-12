b8a9cfcdf79375cbe666c059e64e8356325cb94a
shashikanuwan <kumararanweera1999@gmail.com>
2022-09-12 11:23:38 +0530

    fix customer crud

--- resources/views/components/fetch-customer.blade.php
+++ resources/views/components/fetch-customer.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-col justify-center h-full">
         <div class="w-full max-w-5xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
             <header class="px-5 py-4 border-b border-gray-200">
-                <h2 class="font-semibold text-gray-800">All Distributors</h2>
+                <h2 class="font-semibold text-gray-800">All Customers</h2>
             </header>
             <div class="p-3">
                 <div class="overflow-x-auto">
