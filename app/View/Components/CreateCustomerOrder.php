86159de67892c20a64fafda97b1e943c2b2b3fb1
shashikanuwan <kumararanweera1999@gmail.com>
2022-09-14 16:53:00 +0530

    clean code

--- app/View/Components/CreateCustomerOrder.php
+++ app/View/Components/CreateCustomerOrder.php
@@ -3,8 +3,6 @@
 namespace App\View\Components;
 
 use App\Models\Customer;
-use App\Models\Order;
-use App\Models\Product;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\Component;
@@ -17,7 +15,6 @@ class CreateCustomerOrder extends Component
     public function __construct()
     {
         $this->customers = Customer::query()->where('user_id', Auth::user()->id)->get();
-        $this->products = Product::query()->get();
         $this->orders = Auth::user()->orders;
     }
 
